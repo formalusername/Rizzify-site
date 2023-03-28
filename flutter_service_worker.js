276445d20ca02a5d4ea9c69d05b84f67a111820d
formalusername <informal.username@gmail.com>
2023-03-28 13:06:01 +0200

    added secret message to secret page

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "e345e842aeed8ff14d78e45f91bb4179",
-"index.html": "2fa4628e3ed72d3f551e6c3414ba1397",
-"/": "2fa4628e3ed72d3f551e6c3414ba1397",
-"main.dart.js": "5bf1776f7b1f6e43f23f84544bb1c7bf",
+"index.html": "873de2f4d3b9372d9b94b00f22489ea1",
+"/": "873de2f4d3b9372d9b94b00f22489ea1",
+"main.dart.js": "01009ebd8584750ea8123539e98ad259",
 "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
